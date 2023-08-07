0c7c813c5b1a34d0ba685c30b7098034c992d548
Stephen Crocker <stephen.crocker@pallas.uk>
2023-08-07 22:33:25 +0200

    Updated the system with various tests

--- src/components/common/common.helper.ts
+++ src/components/common/common.helper.ts
@@ -20,10 +20,17 @@ export const CompareRBTRow = <TData extends Record<string, unknown>>(
   return comparison;
 };
 
+/**
+ * Used to remove a value from the supplied array and return the rest of the array contents.
+ *
+ * @param filterValue the value to remove from the array
+ * @param filters the values to be processed.
+ * @returns  an empty string if the supplied
+ */
 export const RemoveFilterFromArray = (filterValue: string, filters: string[]) => {
   const result: string[] = [];
 
-  if (Array.isArray(result)) {
+  if (Array.isArray(filters)) {
     for (let index = 0; index < filters.length; index++) {
       const filter = filters[index];
       if (filter !== filterValue) {
---
+++ src/components/common/common.test.ts
@@ -0,0 +1,75 @@
+import { describe, expect, test } from '@jest/globals';
+
+import type { SimpleDataType } from './common.testdata';
+import { CompareRBTRow, RemoveFilterFromArray } from './common.helper';
+import { RBTRow } from './common.types';
+
+describe('CompareRBTRow', () => {
+  test('Equal', () => {
+    const rowA: RBTRow<SimpleDataType> = {
+      position: 0,
+      filters: [],
+      data: { firstName: 'Joe', surname: 'Bloggs', age: 24 },
+    };
+    const rowB: RBTRow<SimpleDataType> = {
+      position: 0,
+      filters: [],
+      data: { firstName: 'Jane', surname: 'Doe', age: 36 },
+    };
+
+    expect(CompareRBTRow<SimpleDataType>(rowA, rowB)).toBe(0);
+  });
+
+  test('A is Lower', () => {
+    const rowA: RBTRow<SimpleDataType> = {
+      position: 0,
+      filters: [],
+      data: { firstName: 'Joe', surname: 'Bloggs', age: 24 },
+    };
+    const rowB: RBTRow<SimpleDataType> = {
+      position: 1,
+      filters: [],
+      data: { firstName: 'Jane', surname: 'Doe', age: 36 },
+    };
+
+    expect(CompareRBTRow<SimpleDataType>(rowA, rowB)).toBe(-1);
+  });
+
+  test('A is Greater', () => {
+    const rowA: RBTRow<SimpleDataType> = {
+      position: 1,
+      filters: [],
+      data: { firstName: 'Joe', surname: 'Bloggs', age: 24 },
+    };
+    const rowB: RBTRow<SimpleDataType> = {
+      position: 0,
+      filters: [],
+      data: { firstName: 'Jane', surname: 'Doe', age: 36 },
+    };
+
+    expect(CompareRBTRow<SimpleDataType>(rowA, rowB)).toBe(1);
+  });
+});
+
+describe('RemoveFilterFromArray', () => {
+  test('Happy Path', () => {
+    const supplied = ['abc', 'def', 'ghi'];
+    const filter = 'abc';
+    const expected = ['def', 'ghi'];
+
+    expect(RemoveFilterFromArray(filter, supplied)).toStrictEqual(expected);
+  });
+
+  test('Empty Array', () => {
+    const filter = 'abc';
+    expect(RemoveFilterFromArray('', [])).toStrictEqual([]);
+  });
+
+  test('Empty Filter Value', () => {
+    const supplied = ['abc', 'def', 'ghi'];
+    const filter = '';
+    const expected = ['abc', 'def', 'ghi'];
+
+    expect(RemoveFilterFromArray(filter, supplied)).toStrictEqual(expected);
+  });
+});
