e5fd33e3623390bba4ff172e74342f170ee63a17
Stephen Crocker <stephen.crocker@pallas.uk>
2023-11-21 23:09:00 +0100

    Moved file

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-export type {
-  RBTStyleOptions,
-  RBTData,
-  RBTColumnSizing,
-  RBTColumnDefs,
-  RBTMenuBarOptions,
-  RBTMenuBarStyleOptions,
-  RBTRowControlStyleOptions,
-  RBTRowControlOptions,
-  RBTSearchOptions,
-  RBTOptions,
-  RBTableHeadOptions,
-  RBTablePlaceholderOptions,
-} from './components';
+export type { RBTStyleOptions, RBTData, RBTColumnSizing, RBTColumnDefs } from './common';
+export { CompareRBTRow, RemoveFilterFromArray } from './common';
 
-export {
-  RBTable,
-  RBTableHead,
-  RBTablePlaceholder,
-  RBTMenuBar,
-  RBTRowControls,
-  RBTSearch,
-} from './components';
+export type { RBTMenuBarOptions, RBTMenuBarStyleOptions } from './menubar';
+export { RBTMenuBar } from './menubar';
+
+export type { RBTRowControlStyleOptions, RBTRowControlOptions } from './rowcontrols';
+export { RBTRowControls } from './rowcontrols';
+
+export type { RBTSearchOptions } from './search';
+export { RBTSearch } from './search';
+
+export type { RBTOptions } from './table';
+export { RBTable } from './table';
+
+export { RBTableBody } from './tableBody';
+export type { RBTableCellProps } from './tableBody';
+
+export type { RBTableHeadStyleOptions, RBTableHeadOptions } from './tableHead';
+export { RBTableHead } from './tableHead';
+
+export type { RBTablePlaceholderOptions } from './tablePlaceholder';
+export { RBTablePlaceholder } from './tablePlaceholder';
