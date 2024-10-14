a355d3d3e1221c0812f5b7e0970ac7842422ecc4
Stephen Crocker <stephen.crocker@pallas.uk>
2024-10-14 22:30:44 +0100

    Updates with tests with website-core to get the library as loading again

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,21 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
-  core: {
-    builder: '@storybook/builder-vite',
-  },
-  framework: '@storybook/react-vite',
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
+    '@storybook/addon-webpack5-compiler-swc',
+    '@chromatic-com/storybook'
+  ],
+  framework: '@storybook/react-webpack5',
   docs: {
-    autodocs: 'tag',
-    defaultName: 'Documentation',
+    defaultName: 'Documentation'
   },
   typescript: {
-    check: false,
+    check: true,
     reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {}, // Available only when reactDocgen is set to 'react-docgen-typescript'
     skipCompiler: false,
