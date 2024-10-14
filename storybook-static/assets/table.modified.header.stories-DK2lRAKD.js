e1ba8ea93830e0b3772e06da90fcd0f5df619ca3
pallasCI <continuous.integration@pallas.uk>
2024-10-14 21:37:41 +0000

    Updates

--- storybook-static/assets/table.modified.header.stories-DK2lRAKD.js
+++
@@ -1,6 +0,0 @@
-import{S as n,j as r}from"./common.testdata-DhpCxAve.js";import{R as d}from"./table.components-DpgCJLwO.js";import{P as c}from"./person-DeWvC7_k.js";const i={title:"react-bootstrap-table/Table",component:d,tags:["autodocs"]},m=[{accessorKey:"firstName",Header:a=>p()},{accessorKey:"age",Header:a=>l(a)}],l=({id:a})=>r.jsx("h1",{id:a,className:"text-center",children:"Age"}),p=()=>r.jsxs(r.Fragment,{children:[r.jsx(c,{})," First Name"]}),e={args:{columns:m,data:n}};var s,t,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    columns: SimpleColumnDefs,
-    data: SimpleDataset
-  }
-}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const u=["ModifiedHeaderFn"],b=Object.freeze(Object.defineProperty({__proto__:null,ModifiedHeaderFn:e,__namedExportsOrder:u,default:i},Symbol.toStringTag,{value:"Module"}));export{e as M,b as T};
